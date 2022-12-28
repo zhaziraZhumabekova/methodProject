866405d9b2941806d1b13d6bda9b48bc7a881e1d
Toshiba <zhazira9818@gmail.com>
2022-12-29 02:47:57 +0600

    q

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] arrayName = {"name1", "name2", "name3"};
+        for (String a : arrayName){
+            System.out.println(" meni seni jakshu korom : " + a);
+            System.out.println();
+        }
+
+
+//    static int someNumbers(int...sum){
+//        int count = 0;
+//        for (int i = 0; i < sum.length; i++) {
+//            count += sum [i];
+//
+//        }
+//        return count;
+    }
+}
+
